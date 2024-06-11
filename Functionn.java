6b8a6b6685c3a5de0f841b30d3827692a72b8f33
Shreya070303 <maneshreya0703@gmail.com>
2024-06-11 14:59:14 +0530

    Add files via upload
    This file is Calling function with parameter

---
+++ Functionn.java
@@ -0,0 +1,18 @@
+package statement;
+
+public class Functionn {
+	public  static void add() {
+		int a=10;
+		int b=10;
+		int total = a+b;
+	
+	System.out.println("addition :"+total);
+
+}
+	public static void sub(int a,int b) {
+		//int c =10;
+		//int d=9;
+		int total =a-b;
+		System.out.println("Substract :"+total);
+	}
+}
---
+++ testmethod.java
@@ -0,0 +1,13 @@
+package statement;
+
+public class testmethod {
+
+	public static void main(String[] args) {
+	 System.out.println("Helloo");
+	 Functionn fn = new Functionn();
+	 Functionn.add();
+	 Functionn.sub(18,12);// funtn with paramenter
+
+	}
+
+}
